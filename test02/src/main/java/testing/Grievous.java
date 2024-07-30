931f9fe5af3e1c966324ad096f3053592113a58a
matti <matti.pro17@gmail.com>
2024-07-30 22:04:39 +0200

    grievous

---
+++ test02/src/main/java/testing/Grievous.java
@@ -0,0 +1,14 @@
+package testing;
+
+public class Grievous {
+
+
+	public static void main(String[] args) {
+
+		System.out.println("you fool");
+		System.out.println("i've been trained in your jedi arts by count doku");
+	}
+
+}
+
+
